42e99f7722250e521398ece316e0b8b611b06d3a
Hephaestus <yassinesindel@gmail.com>
2022-07-21 14:27:14 +0100

    fix 2

--- src/app/server.js
+++ src/app/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/gestionadmissionfrontend'));
+app.use(express.static('./dist/genstionadmissionfrontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/gestionadmissionfrontend/'}),
+    res.sendFile('index.html', {root: 'dist/genstionadmissionfrontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
