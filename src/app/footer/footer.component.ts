d44f8ba55a13edd38acc0cbe3654a03589f4acc4
Hephaestus <96309227+Hephaestus27@users.noreply.github.com>
2022-07-21 19:04:07 +0200

    Update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    <footer class="footer" style="padding:30px;margin-top:20px;">
   <div class="content has-text-centered">
     <p>
-      <strong>University</strong> by <a href="https://github.com/Hephaestus27">Yassine SINDEL</a> and <a href="https://github.com/fati-es"> Fatima Zahra Essbai</a> The source code is licensed
+      <strong>University</strong> Project , The source code is licensed
     </p>
   </div>
 </footer>
