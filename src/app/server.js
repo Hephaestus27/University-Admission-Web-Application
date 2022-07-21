3f894697faf6b17ecdfa5e39206090a760c67943
Hephaestus <yassinesindel@gmail.com>
2022-07-21 14:26:12 +0100

    fix

--- src/app/server.js
+++ src/app/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-java-project-4'));
+app.use(express.static('./dist/gestionadmissionfrontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-java-project-4/'}),
+    res.sendFile('index.html', {root: 'dist/gestionadmissionfrontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
